f73f87acdfdd9347576825f07fe1ed89e38bc03f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-07-28 22:51:15 +0200

    "Fix" the tab key in Org and Evil mode

--- early-init.el
+++ early-init.el
@@ -1,2 +1,5 @@
 ;; Disable package.el in favor of straight.el
 (setq package-enable-at-startup nil)
+
+;; Disable C-i to jump forward to restore TAB functionality in Org mode.
+(setq evil-want-C-i-jump nil)
