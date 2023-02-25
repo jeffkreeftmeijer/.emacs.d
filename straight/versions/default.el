8bbe22bdd774488cbb305011446231acadb6fce7
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-02-25 10:21:35 +0100

    Check in straight versions file
        M-x straight-freeze versions

---
+++ straight/versions/default.el
@@ -0,0 +1,8 @@
+(("el-get" . "bc4299031e634b514eceb472b6b02432c2bb86ba")
+ ("emacsmirror-mirror" . "d391b480387d216772e6899cda7e9d7ca110cca4")
+ ("gnu-elpa-mirror" . "c1eac9ab251d13d9a37cfba975246f30878ee2b8")
+ ("melpa" . "2e40e3364a5dec38672753093c28ae97d85de189")
+ ("nongnu-elpa" . "9e236b82a2da24bb9983e2f63061323354d6ecfe")
+ ("straight.el" . "3eca39dfc6797243ec7d1c6a7d45142407f73f88")
+ ("use-package" . "77945e002f11440eae72d8730d3de218163d551e"))
+:gamma
