c1f4dda6d4e30f463ca088eac0c3f658de1baf10
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-02-27 21:38:28 +0100

    Turn on org-roam-db-autosync-mode

--- org-roam.el
+++ org-roam.el
@@ -1,3 +1,4 @@
 ;; Org-roam
 (use-package org-roam
-  :init (setq org-roam-directory (file-truename "~/notes")))
+  :init (setq org-roam-directory (file-truename "~/notes"))
+  :config (org-roam-db-autosync-mode))
