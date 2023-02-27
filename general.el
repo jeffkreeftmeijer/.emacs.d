087d09beb64f5266e2dbdbadf733d3d14e21205e
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-02-27 17:17:24 +0100

    Fix the inhibit-startup-echo-area-message setting

--- general.el
+++ general.el
@@ -4,10 +4,13 @@
 ;; Save minibuffer history
 (savehist-mode 1)
 
+;; Disable the startup message
+(put 'inhibit-startup-echo-area-message 'saved-value t)
+
 (setq
  ;; Disable the startup screen
  inhibit-startup-screen t
  ;; Disable the startup message
- inhibit-startup-echo-area-message t
+ inhibit-startup-echo-area-message (user-login-name)
  ;; Store all backup files in =~/.emacs.d/backups/
  backup-directory-alist '(("." . "~/.emacs.d/backups")))
