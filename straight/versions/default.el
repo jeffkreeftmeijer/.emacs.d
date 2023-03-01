efb69c18ef1c121fe79758df88102fc8d7de42ef
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-01 10:06:21 +0100

    Add org to straight versions file

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -6,6 +6,7 @@
  ("goto-chg" . "278cd3e6d5107693aa2bb33189ca503f22f227d0")
  ("melpa" . "2e40e3364a5dec38672753093c28ae97d85de189")
  ("nongnu-elpa" . "9e236b82a2da24bb9983e2f63061323354d6ecfe")
+ ("org" . "351279ffaead08398002fd29801eaad688ccf239")
  ("org-auto-tangle" . "817eabf902e759e96782bdc54d2dab36c4a2c5ab")
  ("straight.el" . "3eca39dfc6797243ec7d1c6a7d45142407f73f88")
  ("use-package" . "77945e002f11440eae72d8730d3de218163d551e"))
