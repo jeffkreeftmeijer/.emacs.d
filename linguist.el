b7c245fc1b29fec645fa5a2aed21b4accf5533fa
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-01 14:57:07 +0100

    Add yaml-mode

--- linguist.el
+++ linguist.el
@@ -1,2 +1,6 @@
 (defun linguist--loaded-message (mode)
   (message (concat "Linguist: Loaded " mode ".")))
+
+(use-package yaml-mode
+  :config (linguist--loaded-message "yaml-mode")
+  :mode ("\\.\\(e?ya?\\|ra\\)ml\\'" . yaml-mode))
