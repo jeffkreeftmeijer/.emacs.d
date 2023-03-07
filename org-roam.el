d2859abbdfc8cb67ccf769f8ab52b37d3d25da5d
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-07 17:03:58 +0100

    Remove dates from org-roam filenames, add them to note contents

--- org-roam.el
+++ org-roam.el
@@ -3,4 +3,10 @@
   :init
   (setq org-roam-directory (file-truename "~/notes"))
   :config
-  (org-roam-db-autosync-mode))
+  (org-roam-db-autosync-mode)
+  :custom
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "%?"
+      :target (file+head "${slug}.org" "#+title: ${title}\n#+date: %<%Y-%m-%d>")
+      :unnarrowed t))))
