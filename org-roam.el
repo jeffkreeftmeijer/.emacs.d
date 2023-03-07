ccc278cf6161c520c43ae10084fbd27fdf623663
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-07 17:02:21 +0100

    Clean up configuration blocks in emacs-org-roam.org

--- org-roam.el
+++ org-roam.el
@@ -1,4 +1,6 @@
 ;; Org-roam
 (use-package org-roam
-  :init (setq org-roam-directory (file-truename "~/notes"))
-  :config (org-roam-db-autosync-mode))
+  :init
+  (setq org-roam-directory (file-truename "~/notes"))
+  :config
+  (org-roam-db-autosync-mode))
