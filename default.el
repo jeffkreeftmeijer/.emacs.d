e20bd2935554937e7b97cc6aa963418f21eb7c97
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-18 09:59:00 +0100

    Add evil-collection

--- default.el
+++ default.el
@@ -1,6 +1,17 @@
 (use-package evil
+  :init
+  (setq evil-want-keybinding nil)
   :config
   (evil-mode 1))
+
+(use-package evil-collection
+  :commands
+  (evil-collection-eshell-setup evil-collection-magit-setup))
+
+(use-package eshell
+  :defer t
+  :config
+  (evil-collection-eshell-setup))
 
 (use-package magit
   :defer t)
