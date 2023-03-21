d696b18e504b86fcd8be404af7539219d0b246a3
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-21 16:09:44 +0100

    Remove the scratch buffer message

--- default.el
+++ default.el
@@ -3,6 +3,7 @@
   (scroll-bar-mode -1)
   (tool-bar-mode -1)
   (menu-bar-mode -1)
+  (setq initial-scratch-message nil)
   (set-face-attribute 'default nil :font "SF Mono-15"))
 
 (use-package evil
