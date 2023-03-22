02f49ae6ee9d027d905ac8a4d4d0a9b784844aa9
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-03-22 15:27:01 +0100

    Add linguist.el
    For some reason, Linguist's dependencies aren't extracted from its
    Package-Requires, add the dependencies to extraEmacsPackeges manually.

--- flake.nix
+++ flake.nix
@@ -22,6 +22,21 @@
         config = ./default.el;
         defaultInitFile = true;
         package = pkgs.emacsGit;
+        extraEmacsPackages = epkgs: [
+          epkgs.dockerfile-mode
+          epkgs.elixir-mode
+          epkgs.git-modes
+          epkgs.ledger-mode
+          epkgs.markdown-mode
+          epkgs.nix-mode
+          epkgs.yaml-mode
+        ];
+
+        override = epkgs: epkgs // {
+          linguist = pkgs.callPackage ../pkgs/linguist.el {
+            trivialBuild = epkgs.trivialBuild;
+          };
+        };
       });
     });
 
