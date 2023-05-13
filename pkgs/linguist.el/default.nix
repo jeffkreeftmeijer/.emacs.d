bf1e85c65aae8cc1250307a38130f368be423e6f
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-05-13 16:23:16 +0200

    Load elixir mode via input

--- pkgs/linguist.el/default.nix
+++ pkgs/linguist.el/default.nix
@@ -1,4 +1,4 @@
-{lib, fetchFromGitea, trivialBuild, ...}:
+{ lib, fetchFromGitea, trivialBuild, elixir-mode, ...}:
 
 trivialBuild {
   pname = "linguist";
@@ -10,4 +10,6 @@ trivialBuild {
     rev = "275182957c345bd2d88cee30633c14cace7e56e6";
     sha256 = "sha256-NRk9tM0U6junhgK/P0yb0haMzkm5IVWMgAa48KTspoQ=";
   };
+
+  packageRequires = [ elixir-mode ];
 }
