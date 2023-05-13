29fbd104db790a62ed4e0de37fedce2178d5a229
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-05-13 23:53:02 +0200

    Add pkgs/linguist

---
+++ pkgs/linguist.el/default.nix
@@ -0,0 +1,13 @@
+{ lib, fetchFromGitea, trivialBuild, ...}:
+
+trivialBuild {
+  pname = "linguist";
+
+  src = fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "jkreeftmeijer";
+    repo = "linguist.el";
+    rev = "275182957c345bd2d88cee30633c14cace7e56e6";
+    sha256 = "sha256-NRk9tM0U6junhgK/P0yb0haMzkm5IVWMgAa48KTspoQ=";
+  };
+}
