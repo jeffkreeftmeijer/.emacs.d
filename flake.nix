915f98ce3be3d86c4e6d4c1895cddb005fe5e8f0
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-05-13 14:09:41 +0200

    Add linguist.el as pkg

--- flake.nix
+++ flake.nix
@@ -27,18 +27,9 @@
             tree-sitter-heex
           ];
         };
-        extraEmacsPackages = epkgs: [
-          epkgs.dockerfile-mode
-          epkgs.elixir-mode
-          epkgs.git-modes
-          epkgs.ledger-mode
-          epkgs.markdown-mode
-          epkgs.nix-mode
-          epkgs.yaml-mode
-        ];
 
         override = epkgs: epkgs // {
-          linguist = pkgs.callPackage ../pkgs/linguist.el {
+          linguist = pkgs.callPackage pkgs/linguist.el {
             trivialBuild = epkgs.trivialBuild;
           };
         };
