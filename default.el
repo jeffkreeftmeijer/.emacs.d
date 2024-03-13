fcf082b5fdc4720c9b3eab4c59726ea3f7cdab21
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-13 16:00:28 +0100

    Add rust-mode

--- default.el
+++ default.el
@@ -154,6 +154,11 @@
   :ensure t
   :mode ("\\.mdx\\'" . markdown-mode))
 
+(use-package rust-mode
+  :config (message "Loaded rust-mode")
+  :defer t
+  :ensure t)
+
 (use-package emacs
   :custom
   (org-babel-load-languages '((emacs-lisp . t)
