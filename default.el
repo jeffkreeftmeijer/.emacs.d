8de747ce35cb3fbf9d49df2aa72e3b4b1f468b10
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-27 17:55:00 +0100

    Use SF Pro for variable width text

--- default.el
+++ default.el
@@ -19,7 +19,8 @@
 
 (use-package emacs
   :init
-  (set-face-attribute 'default nil :family "SF Mono" :height 140))
+  (set-face-attribute 'default nil :family "SF Mono" :height 140)
+  (set-face-attribute 'variable-pitch nil :family "SF Pro" :height 140))
 
 (use-package modus-themes
   :ensure t)
