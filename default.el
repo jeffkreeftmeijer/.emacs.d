e2b1a216de32c1561bcdee41543e7282a1d9b13f
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-01 00:51:44 +0200

    Add font configuration

--- default.el
+++ default.el
@@ -3,3 +3,13 @@
   (scroll-bar-mode -1)
   (tool-bar-mode -1)
   (menu-bar-mode -1))
+
+(use-package faces
+  :init
+  (set-face-attribute 'default nil :family "SF Mono" :height 140)
+  (set-face-attribute 'fixed-pitch nil :family "SF Mono" :height 140)
+  (set-face-attribute 'variable-pitch nil :family "SF Pro" :height 170)
+  :custom
+  (face-font-family-alternatives
+    '(("SF Mono" "Menlo" "monospace")
+      ("SF Pro" "Helvetica" "Arial"))))
