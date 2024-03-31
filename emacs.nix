c9775e955e341383928946c3bf65d8ffb5a435dd
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-31 23:11:04 +0200

    Add empty default.el

--- emacs.nix
+++ emacs.nix
@@ -1,3 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.emacs
+let
+  epkgs = pkgs.emacsPackages;
+  emacsWithPackages = (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages;
+
+  default = epkgs.trivialBuild {
+    pname = "default";
+    src = pkgs.writeText "default.el" (builtins.readFile ./default.el);
+    version = "0.1.0";
+  };
+in
+emacsWithPackages [
+  default
+]
+
