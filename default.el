f7891309c459e50d95a67202f35a0552f5f579bd
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-02 22:01:11 +0200

    Switch from SF Mono and SF Pro to Iosevka and Iosevka Aile

--- default.el
+++ default.el
@@ -6,13 +6,13 @@
 
 (use-package faces
   :init
-  (set-face-attribute 'default nil :family "SF Mono" :weight 'regular :width 'regular :height 140)
-  (set-face-attribute 'fixed-pitch nil :family "SF Mono" :weight 'regular :width 'regular :height 140)
-  (set-face-attribute 'variable-pitch nil :family "SF Pro" :weight 'regular :width 'regular :height 170)
+  (set-face-attribute 'default nil :family "Iosevka" :weight 'regular :width 'expanded :height 150)
+  (set-face-attribute 'fixed-pitch nil :family "Iosevka" :weight 'regular :width 'expanded :height 150)
+  (set-face-attribute 'variable-pitch nil :family "Iosevka Aile" :weight 'regular :width 'expanded :height 150)
   :custom
   (face-font-family-alternatives
-    '(("SF Mono" "Menlo" "monospace")
-      ("SF Pro" "Helvetica" "Arial")))
+    '(("Iosevka" "SF Mono" "Menlo" "monospace")
+      ("Iosevka Aile" "SF Pro" "Helvetica" "Arial")))
   :hook
   (org-mode . variable-pitch-mode)
   (markdown-mode . variable-pitch-mode))
