a0824725dbf0c7a98c5366eb8bd5fd8795f94361
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-04 00:31:41 +0200

    Add packages.aarch64-darwin.default

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
     system = "aarch64-darwin";
     pkgs = import nixpkgs { inherit system; };
   in {
+    packages.${system}.default = pkgs.callPackage ./configured-emacs.nix {};
+
     devShell."${system}" = pkgs.mkShell {
       buildInputs = [
         pkgs.pre-commit
