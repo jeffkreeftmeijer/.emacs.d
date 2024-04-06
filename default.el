24fd747a6e38ccae0539d9d5751257c290141c7b
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-06 18:03:58 +0200

    Fix indentation for jk/set-face-font calls

--- default.el
+++ default.el
@@ -11,9 +11,9 @@
      face nil
      :family family :weight 'regular :width 'expanded :height 150))
 
-    (jk/set-face-font 'default "Iosevka")
-    (jk/set-face-font 'fixed-pitch "Iosevka")
-    (jk/set-face-font 'variable-pitch "Iosevka Aile")
+  (jk/set-face-font 'default "Iosevka")
+  (jk/set-face-font 'fixed-pitch "Iosevka")
+  (jk/set-face-font 'variable-pitch "Iosevka Aile")
   :custom
   (face-font-family-alternatives
     '(("Iosevka" "SF Mono" "Menlo" "monospace")
