b3a52e9e7fad7b69291d1dae88114169100f306c
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-10 18:22:04 +0200

    Add emacs-overlay in flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,18 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    emacs-overlay.url = "github:nix-community/emacs-overlay/f7fcac1403356fd09e2320bc3d61ccefe36c1b91";
   };
   outputs = {
     nixpkgs,
     flake-utils,
+    emacs-overlay,
     ...
   }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs { inherit system; };
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ emacs-overlay.overlay ];
+    };
     configured-emacs = pkgs.callPackage ./configured-emacs.nix {};
   in {
     packages.configured-emacs = configured-emacs;
