68c4428ae497ed8431b470c55640edc3a1db7ecd
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-25 17:15:00 +0200

    Add general Org export configuration options

--- default.el
+++ default.el
@@ -202,6 +202,11 @@ end tell"))
 
 (use-package ox-org
   :custom
+  org-export-with-smart-quotes t
+  org-export-with-entities nil
+  org-export-headline-levels 5
+  org-export-with-toc nil
+  org-export-section-numbers nil
   org-html-doctype "html5"
   org-html-html5-fancy t
   org-html-container-element "section"
