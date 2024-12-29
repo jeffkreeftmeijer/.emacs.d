480ca3e4e28129537704b6cb8aaf504ed47a7486
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-12-29 08:16:54 +0100

    Remove mac-specific patches

--- configured-emacs.nix
+++ configured-emacs.nix
@@ -7,23 +7,6 @@
 } }:
 
 pkgs.emacsWithPackagesFromUsePackage {
-  package = (
-    pkgs.emacs-git.overrideAttrs(old: {
-      patches = old.patches ++ [
-        ./patches/system-appearance.patch
-        ./patches/round-undecorated-frame.patch
-        ./patches/poll.patch
-        ./patches/fix-window-role.patch
-      ];
-
-      buildInputs = old.buildInputs ++ [
-        pkgs.darwin.apple_sdk.frameworks.WebKit
-      ];
-
-      configureFlags = old.configureFlags ++ ["--with-xwidgets"];
-    })
-  );
-
   config = ./default.el;
   defaultInitFile = true;
 }
