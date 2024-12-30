b12872f65e0785dd203b34ca5db950cd94e762e3
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-12-30 08:54:35 +0100

    wip

--- configured-emacs.nix
+++ configured-emacs.nix
@@ -1,12 +1,29 @@
 { pkgs ? import <nixpkgs> {
   overlays = [
     (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/97678931872b1bad445ed341e083c09025b4f0e7.tar.gz;
+      url = https://github.com/nix-community/emacs-overlay/archive/f7fcac1403356fd09e2320bc3d61ccefe36c1b91.tar.gz;
     }))
   ];
 } }:
 
 pkgs.emacsWithPackagesFromUsePackage {
+  package = (
+    pkgs.emacs-git.overrideAttrs(old: {
+      patches = old.patches ++ [
+        ./patches/system-appearance.patch
+        ./patches/round-undecorated-frame.patch
+        ./patches/poll.patch
+        ./patches/fix-window-role.patch
+      ];
+
+      buildInputs = old.buildInputs ++ [
+        pkgs.darwin.apple_sdk.frameworks.WebKit
+      ];
+      
+      configureFlags = old.configureFlags ++ ["--with-xwidgets"];
+    })
+  );
+
   config = ./default.el;
   defaultInitFile = true;
 }
